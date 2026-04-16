e3f31ae4c8dfaac91e103fc157375f41dcc4e7bf
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-04-16 12:25:38 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "lexer.h"
+#include "codegen.h"
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -13,12 +14,22 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    Token t;
     printf("--- தமிழி கம்பைலர் (v0.1) ---\n");
 
+    // 1. LLVM Engine-a initialize pannuvom
+    tamizhi_codegen_init();
+
+    // 2. Entry point (main function) create pannuvom
+    tamizhi_generate_entry();
+
+    // 3. Tokens-a analyze panni screen-la print pannuvom (Temporary)
+    Token t;
     while ((t = get_next_token(file)).type != T_EOF) {
         printf("வகை: %d | மதிப்பு: %s\n", t.type, t.value);
     }
+
+    // 4. Mudivila LLVM IR-a print panni finish pannuvom
+    tamizhi_codegen_finish();
 
     fclose(file);
     printf("\nஆய்வு முடிந்தது.\n");
