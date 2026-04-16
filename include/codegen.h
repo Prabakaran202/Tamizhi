72690e1346092bde31abcfff5ecd99a1b773fb4c
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-04-16 13:21:59 +0530

    Update codegen.h

--- include/codegen.h
+++ include/codegen.h
@@ -1,8 +1,9 @@
 #ifndef CODEGEN_H
 #define CODEGEN_H
+#include <llvm-c/Core.h>
 
 void tamizhi_codegen_init();
 void tamizhi_generate_entry();
 void tamizhi_codegen_finish();
-
+void tamizhi_gen_var_decl(char* name, int initial_value);
 #endif
