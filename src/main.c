30439d6d86153a204e4f14884ef05fd0e994ae46
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-04-16 13:28:13 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
 
     // 2. Main entry point create pannuvom
     tamizhi_generate_entry();
-    tamizhi_gen_loop_test(1000000);-a
+    tamizhi_gen_loop_test(1000000);
 
 
     // 3. Oru dummy variable create panni LLVM memory-a check pannuvom
