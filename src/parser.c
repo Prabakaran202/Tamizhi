a181bcafc05cae689be47a1d4429e6fea97d608a
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-04-18 18:16:21 +0530

    Update parser.c

--- src/parser.c
+++ src/parser.c
@@ -50,7 +50,7 @@ void parse(FILE *file) {
 
         // 3. 'சு' (Loop) handle pannuvom
         else if (strcmp(t.value, "சு") == 0) {
-            printf(stderr,"[Parser] Loop detected! Triggering 1M Loop Test...\n");
+            fprintf(stderr,"[Parser] Loop detected! Triggering 1M Loop Test...\n");
             tamizhi_gen_loop_test(1000000);
         }
     }
