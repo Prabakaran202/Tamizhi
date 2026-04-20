40cf26f714908d1a4db39c41085438d6876b3e5e
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-04-20 12:06:09 +0530

    Update codegen.c

--- src/codegen.c
+++ src/codegen.c
@@ -1,4 +1,4 @@
-#include "codegen.h"
+/*#include "codegen.h"
 #include <llvm-c/Core.h>
 #include <llvm-c/Analysis.h>
 #include <stdio.h>
@@ -131,5 +131,84 @@ void tamizhi_codegen_finish() {
     printf("%s", ir_string); // <--- Ithu thaan loop.ll kulla code-ah anuppum
     LLVMDisposeMessage(ir_string);
 
+    LLVMDisposeBuilder(builder);
+}*/
+#include "codegen.h"
+#include <llvm-c/Core.h>
+#include <llvm-c/Analysis.h>
+#include <stdio.h>
+
+LLVMModuleRef module;
+LLVMBuilderRef builder;
+LLVMTypeRef printf_type;
+LLVMValueRef printf_func;
+LLVMValueRef i_ptr; // Global pointer for the loop variable
+
+void tamizhi_codegen_init() {
+    module = LLVMModuleCreateWithName("tamizhi_engine");
+    builder = LLVMCreateBuilder();
+
+    // Define printf type: int printf(char*, ...)
+    LLVMTypeRef printf_args[] = { LLVMPointerType(LLVMInt8Type(), 0) };
+    printf_type = LLVMFunctionType(LLVMInt32Type(), printf_args, 1, 1);
+    printf_func = LLVMAddFunction(module, "printf", printf_type);
+
+    fprintf(stderr," [Codegen] LLVM Engine initialized.\n");
+}
+
+void tamizhi_generate_entry() {
+    LLVMTypeRef main_func_type = LLVMFunctionType(LLVMInt32Type(), NULL, 0, 0);
+    LLVMValueRef main_func = LLVMAddFunction(module, "main", main_func_type);
+
+    LLVMSetFunctionCallConv(main_func, LLVMCCallConv); 
+    LLVMBasicBlockRef entry = LLVMAppendBasicBlock(main_func, "entry");
+    LLVMPositionBuilderAtEnd(builder, entry);
+}
+
+void tamizhi_gen_print(char* var_name) {
+    LLVMValueRef fmt = LLVMBuildGlobalStringPtr(builder, "%d\n", "fmt");
+    // Load current value from the global i_ptr
+    LLVMValueRef val = LLVMBuildLoad2(builder, LLVMInt32Type(), i_ptr, "load_val");
+    
+    LLVMValueRef args[] = { fmt, val };
+    LLVMBuildCall2(builder, printf_type, printf_func, args, 2, "print_call");
+}
+
+void tamizhi_gen_loop_test(int limit) {
+    LLVMValueRef main_func = LLVMGetBasicBlockParent(LLVMGetInsertBlock(builder));
+
+    LLVMBasicBlockRef cond_block = LLVMAppendBasicBlock(main_func, "loop_cond");
+    LLVMBasicBlockRef body_block = LLVMAppendBasicBlock(main_func, "loop_body");
+    LLVMBasicBlockRef after_block = LLVMAppendBasicBlock(main_func, "loop_after");
+
+    // FIX: Removed 'LLVMValueRef' prefix to assign to the GLOBAL i_ptr
+    i_ptr = LLVMBuildAlloca(builder, LLVMInt32Type(), "i");
+    LLVMBuildStore(builder, LLVMConstInt(LLVMInt32Type(), 0, 0), i_ptr);
+
+    LLVMBuildBr(builder, cond_block);
+    LLVMPositionBuilderAtEnd(builder, cond_block);
+
+    LLVMValueRef i_val = LLVMBuildLoad2(builder, LLVMInt32Type(), i_ptr, "i_val");
+    LLVMValueRef cond = LLVMBuildICmp(builder, LLVMIntSLT, i_val, LLVMConstInt(LLVMInt32Type(), limit, 0), "tmp_cond");
+    LLVMBuildCondBr(builder, cond, body_block, after_block);
+
+    LLVMPositionBuilderAtEnd(builder, body_block);
+    
+    // Call print inside the loop body so we actually see output
+    tamizhi_gen_print("i");
+
+    LLVMValueRef next_val = LLVMBuildAdd(builder, i_val, LLVMConstInt(LLVMInt32Type(), 1, 0), "next_i");
+    LLVMBuildStore(builder, next_val, i_ptr);
+    LLVMBuildBr(builder, cond_block);
+
+    LLVMPositionBuilderAtEnd(builder, after_block);
+}
+
+void tamizhi_codegen_finish() {
+    LLVMBuildRet(builder, LLVMConstInt(LLVMInt32Type(), 0, 0)); 
+    
+    char *ir_string = LLVMPrintModuleToString(module);
+    printf("%s", ir_string); 
+    LLVMDisposeMessage(ir_string);
     LLVMDisposeBuilder(builder);
 }
