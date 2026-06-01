7c7a0ddb03ca3bb4596b7e88967c978fcb7e5afb
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-06-01 23:37:16 +0530

    Update setup.py

--- tamizhi-py/setup.py
+++ tamizhi-py/setup.py
@@ -1,1 +1,13 @@
-.
+from setuptools import setup, find_packages
+
+setup(
+    name="tamizhi",
+    version="v0.0.2",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "tamizhi=tamizhi.cli:main",
+        ],
+    },
+)
