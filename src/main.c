b3e669dc960134439abc88cbee9140655b079c98
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-06-01 23:48:51 +0530

    Update TAMIZHI_VERSION to v0.0.3

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include "codegen.h"
 
 // 🌟 வெர்ஷனை குளோபல் மேக்ரோவாக அறிவிக்கிறோம் (இனி இங்க மட்டும் மாத்தினால் போதும் பிரபா!)
-#define TAMIZHI_VERSION "v0.0.2"
+#define TAMIZHI_VERSION "v0.0.3"
 
 // 🌟 தமிழி மொழிக்கான பிரத்தியேக கலைநயமிக்க பேனர் மற்றும் சூழல் தகவல்
 void print_tamizhi_environment() {
