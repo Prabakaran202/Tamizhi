3b7f8e2c2fa795a2f1b5c3df8747203ef5d580d1
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-06-19 15:40:18 +0530

    Add debug mode logging to parser

--- src/parser.c
+++ src/parser.c
@@ -181,8 +181,9 @@ void parse(FILE *file) {
     Token t;
     long main_pos = -1L;
     long footer_pos = -1L;
-
-    fprintf(stderr, "\n[Parser] --- Tamizhi Engine Started ---\n");
+    if (tamizhi_debug_mode) { 
+        fprintf(stderr, "\n[Parser] --- Tamizhi Engine Started ---\n");
+    }
     scan_headers(file);
 
     long check_main = find_function("main");
@@ -248,7 +249,9 @@ void parse(FILE *file) {
             parse_statement(file, t);
         }
     }
-    fprintf(stderr, "[Parser] --- Completed Successfully ---\n");
+    if (tamizhi_debug_mode) { fprintf(stderr, "[Parser] --- Completed Successfully ---\n");
+                            }
+    
 }
 
 // ==========================================================
