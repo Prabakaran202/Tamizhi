e8a94153e8589dfe479f5a6b0971e17c4321699c
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-06-20 11:06:29 +0530

    Update cg_bitcode.c
    update

--- src/codegen/cg_bitcode.c
+++ src/codegen/cg_bitcode.c
@@ -4,10 +4,14 @@ void tamizhi_generate_universal_bitcode(const char* filename) {
     if (LLVMWriteBitcodeToFile(module, filename) != 0) {
         fprintf(stderr, " [Error] Failed to write universal bitcode!\n");
     } else {
+        if (tamizhi_debug_mode){
         fprintf(stderr, " [Universal] Bitcode generated: %s\n", filename);
     }
+    }
     char asm_path[256];
+    if (tamizhi_debug_mode){
     sprintf(asm_path, "storage/output.ll");
+    }
     FILE *f = fopen(asm_path, "w");
     if (f) {
         char *str = LLVMPrintModuleToString(module);
