fccda2f09fc037fef4f11ec72c22e5793f7eb5f0
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-07-03 18:30:24 +0530

    Update cg_http.c
    update

--- src/codegen/cg_http.c
+++ src/codegen/cg_http.c
@@ -1,62 +1,36 @@
-#include "codegen_bridge.h"
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include "codegen.h"
+#include <llvm-c/Core.h>
 #include <stdio.h>
 
-// குளோபல் சாக்கெட் டிஸ்கிரிப்டர்
-int global_server_fd = -1;
+extern LLVMModuleRef module;
+extern LLVMContextRef context;
+extern LLVMBuilderRef builder;
 
 void tamizhi_gen_socket_listen(int port) {
-    // 1. சாக்கெட் உருவாக்குதல்
-    global_server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (global_server_fd == -1) {
-        fprintf(stderr, "[HTTP Error] சாக்கெட்டை உருவாக்க முடியவில்லை!\n");
-        return;
-    }
-
-    // சாக்கெட் ஆப்ஷன்ஸ் (Port-ஐ மீண்டும் பயன்படுத்த அனுமதித்தல்)
-    int opt = 1;
-    setsockopt(global_server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
-
-    // 2. Bind செய்தல்
-    if (bind(global_server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        fprintf(stderr, "[HTTP Error] Bind தோல்வி!\n");
-        return;
-    }
-
-    // 3. Listen செய்தல்
-    if (listen(global_server_fd, 3) < 0) {
-        fprintf(stderr, "[HTTP Error] Listen தோல்வி!\n");
-        return;
+    // 1. LLVM-ல் external ஃபங்ஷனை Declare செய்தல்: void tamizhi_rt_listen(int)
+    LLVMTypeRef param_types[] = { LLVMInt32TypeInContext(context) };
+    LLVMTypeRef func_type = LLVMFunctionType(LLVMVoidTypeInContext(context), param_types, 1, 0);
+    
+    LLVMValueRef func = LLVMGetNamedFunction(module, "tamizhi_rt_listen");
+    if (!func) {
+        func = LLVMAddFunction(module, "tamizhi_rt_listen", func_type);
     }
     
-    printf("[HTTP System] போர்ட் %d-ல் சர்வர் இயங்குகிறது...\n", port);
+    // 2. அந்த ஃபங்ஷனை ரன்-டைமில் கூப்பிடுவதற்கான (Call) இன்ஸ்ட்ரக்ஷனை உருவாக்குதல்
+    LLVMValueRef port_val = LLVMConstInt(LLVMInt32TypeInContext(context), port, 0);
+    LLVMValueRef args[] = { port_val };
+    LLVMBuildCall2(builder, func_type, func, args, 1, "");
 }
 
 void tamizhi_gen_socket_accept() {
-    if (global_server_fd == -1) {
-        fprintf(stderr, "[HTTP Error] சர்வர் இன்னும் தொடங்கப்படவில்லை!\n");
-        return;
-    }
-
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    // 1. LLVM-ல் external ஃபங்ஷனை Declare செய்தல்: void tamizhi_rt_accept()
+    LLVMTypeRef func_type = LLVMFunctionType(LLVMVoidTypeInContext(context), NULL, 0, 0);
     
-    // 4. Accept செய்தல் (கிளையண்ட் கனெக்ஷனுக்காக காத்திருக்கும்)
-    int new_socket = accept(global_server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-    
-    if (new_socket < 0) {
-        fprintf(stderr, "[HTTP Error] Accept தோல்வி!\n");
-        return;
+    LLVMValueRef func = LLVMGetNamedFunction(module, "tamizhi_rt_accept");
+    if (!func) {
+        func = LLVMAddFunction(module, "tamizhi_rt_accept", func_type);
     }
     
-    printf("[HTTP System] புதிய கிளையண்ட் இணைக்கப்பட்டார்!\n");
-    // இங்கே கிளையண்டிற்கு ரெஸ்பான்ஸ் அனுப்பும் லாஜிக்கை அடுத்து சேர்க்கலாம்
-    close(new_socket);
+    // 2. அந்த ஃபங்ஷனை ரன்-டைமில் கூப்பிடுவதற்கான இன்ஸ்ட்ரக்ஷனை உருவாக்குதல்
+    LLVMBuildCall2(builder, func_type, func, NULL, 0, "");
 }
