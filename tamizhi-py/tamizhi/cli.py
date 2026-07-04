33c4deefd65feef134932f1cae7af405be077144
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-07-04 18:11:19 +0530

    Update release URL to use raw content link

--- tamizhi-py/tamizhi/cli.py
+++ tamizhi-py/tamizhi/cli.py
@@ -7,9 +7,8 @@ import shutil
 from pathlib import Path
 
 BINARY_PATH = Path.home() / ".tamizhi" / "bin" / "tamizhi"
-
 RELEASE_URL = (
-    "https://github.com/BackendDeveloperHub/Tamizhi/blob/main/tamizhi-v2.0.2-linux.tar.gz"
+    "https://github.com/BackendDeveloperHub/Tamizhi/raw/main/tamizhi-v2.0.2-linux.tar.gz"
 )
 
 def ensure_binary():
