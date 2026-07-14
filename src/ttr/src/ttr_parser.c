bc1ec3f83d46f62c8497a73bba702f2b91cc8154
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-07-15 00:04:20 +0530

    Rename parse function to ttr_parse

--- src/ttr/src/ttr_parser.c
+++ src/ttr/src/ttr_parser.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../core/parser.h"
+#include "../core/ttr_parser.h"
 
-ASTProgram parse() {
+ASTProgram ttr_parse() {
     ASTProgram program;
     program.count = 0;
 
